3b9f1faa1b60d2b556348cce32ab02727387eed2
alex206 <alexeev108@yandex.ru>
2024-11-06 19:52:20 +0300

    feat: изменен файл wsgi.py и settings.py

--- project/settings.py
+++ project/settings.py
@@ -146,11 +146,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"  # Папка в корне проекта, где будут собираться статические файлы
+STATIC_URL = 'static/'  # Папка в корне проекта, где будут собираться статические файлы
 if 'localhost' in ALLOWED_HOSTS:
-   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Папка для локального проекта
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Папка для локального проекта
 else:
-   STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Папка для сервера
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Папка для сервера
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Место для хранения (на сервере) медиафайлов
